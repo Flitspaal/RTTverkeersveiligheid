c7b8cdb8cae454091b24e81f1ca4b73828a442d6
Frits Duindam <92157304+Flitspaal@users.noreply.github.com>
2022-06-06 15:24:23 +0200

    update code van V1
    added:
     verkeerslicht,
     warning

--- V1.ino
+++ V1.ino
@@ -2,9 +2,25 @@
 #include <LiquidCrystal_I2C.h> //LCD i2c lib
 
 #define POTMETER A0
+#define warning 30
 
-float rr = 0;
-float ss = 0;
+//Defines voor de stoplicht pins
+// SPECIFIEK VOOR DE WEMOS, AANPASSEN DUS
+
+//VERKEERSLICHTEN
+int groenLicht = 12;
+int oranjeLicht = 13;
+int roodLicht = 15;
+
+//fases verkeerslichten
+bool uitgeschakeld = false;
+bool roodFase = false;
+int groenTijd = 6000 ;
+int oranjeTijd = 4500;        //tijd van oranje knipperen, 80km/h weg = 4.5 sec
+int roodTijd = 6000;
+int cyclusTijd = groenTijd + oranjeTijd + roodTijd;
+
+unsigned long timer;
 
 double remWeg(double);
 double potmeter();
@@ -20,16 +36,27 @@ void setup() {
   lcd.print("INFO");
   lcd.setCursor(0,1);
   lcd.print("Screen");
-  
+
+  timer = millis();
+
+  pinMode(groenLicht, OUTPUT);
+  pinMode(oranjeLicht, OUTPUT);
+  pinMode(roodLicht, OUTPUT);  
 }
 
 void loop(){
+ 
+
   double p = potmeter(); // waarde van de potmeter
   double r = remWeg(p); // remweg berekenen
   Serial.print(r);
   Serial.println(" m");
   controllLCD(p,r);
-  delay(1000);
+  stoplicht();
+  if (millis() - timer > cyclusTijd) {
+    timer = millis();
+  }
+
 }
 
 double remWeg(double v){ //calculates remweg
@@ -46,23 +73,49 @@ double potmeter(){
 }
 
 void controllLCD(float s, float r) {
-  if(ss != s || rr != r){
-    ss = s;
-    rr = r;
+
   lcd.clear();// clear previous values from screen
   lcd.print("KM/U");
   lcd.setCursor(7,0);
-  lcd.print(ss);
+  lcd.print(s);
   lcd.setCursor(0,1);
   lcd.print("Remweg");
   lcd.setCursor(7,1);
-  lcd.print(rr);
+  lcd.print(r);
   lcd.setCursor(14,1);
   lcd.print("M");
-    if(r>30){ // als remafstand groter is dan 30 geef dan waarschuwing
-      lcd.setCursor(0,2);
-      lcd.print("---!!---");
+  
+    if(r>warning && roodFase == true){ // als remafstand groter is dan 30 geef dan waarschuwing
+        lcd.setCursor(0,2);
+        lcd.print("---!!---");
     }
-  }
   delay(200);
+}
+
+void stoplicht() {
+
+//lichten op groen
+  if (millis() - timer < groenTijd) { 
+    digitalWrite(groenLicht, HIGH);
+    digitalWrite(oranjeLicht, LOW);
+    digitalWrite(roodLicht, LOW);
+    roodFase = false;
+  }
+
+//lichten op oranje
+  if (millis() - timer > groenTijd && millis() - timer < (groenTijd + oranjeTijd)) {
+    digitalWrite(groenLicht, LOW);
+    digitalWrite(oranjeLicht, HIGH);
+    digitalWrite(roodLicht, LOW);
+    roodFase = true;
+  }
+
+//lichten op rood
+  if (millis() - timer > (groenTijd + oranjeTijd) && millis() - timer < cyclusTijd) {
+    digitalWrite(groenLicht, LOW);
+    digitalWrite(oranjeLicht, LOW);
+    digitalWrite(roodLicht, HIGH);
+    roodFase = true; 
+  }
+
 }
